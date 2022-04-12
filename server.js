821062d926b808bab82f9d1bd2a3c36cd7c7b16d
Vikas Surera <vikas@bitpack.me>
2022-04-12 19:52:15 +0530

    modified meta data

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', function(request, response) {
     const data = '<!DOCTYPE html><html lang="en"><head>'
         + '<meta property="og:title" content="This is title" />'
         + '<meta property="og:description" content="This is description" />'
-        + '<meta property="og:image" content="https://codeat21.com/wp-content/uploads/2021/03/CodeAT21-Logo.png" />'
-        + '<meta property="og:url" content="https://codeat21.com/wp-content/uploads/2021/03/CodeAT21-Logo.png" />'
+        + '<meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Apple-logo.png/640px-Apple-logo.png" />'
+        + '<meta property="og:url" content="https://vikassurera.com" />'
         + '<meta property="og:type" content="article" />'
         + '<meta http-equiv="refresh" content="1; url=https://codeat21.com/wp-content/uploads/2021/03/CodeAT21-Logo.png" />'
         +'</head></html>';
