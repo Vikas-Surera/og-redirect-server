a11e5bc665bc12b67cefd993f92c8cd32f323f12
Vikas Surera <vikas@bitpack.me>
2022-04-12 19:40:52 +0530

    modified express response

--- server.js
+++ server.js
@@ -8,19 +8,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
   console.log('Home page visited!');
-  const filePath = path.join(__dirname, 'public', 'index.html');
-  // read in the index.html file
-  fs.readFile(filePath, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    // replace the special strings with server generated strings
-    data = data.replace(/\$OG_TITLE/g, 'Home Page');
-      data = data.replace(/\$OG_DESCRIPTION/g, "Home page description");
-      data = data.replace(/\$OG_URL/g, "https://codeat21.com/wp-content/uploads/2021/03/CodeAT21-Logo.png");
-    result = data.replace(/\$OG_IMAGE/g, 'https://codeat21.com/wp-content/uploads/2021/03/CodeAT21-Logo.png');
-    return response.send(result);
-  });
+    const data = '<!DOCTYPE html><html lang="en"><head>'
+        + '<meta property="og:title" content="This is title" />'
+        + '<meta property="og:description" content="This is description" />'
+        + '<meta property="og:image" content="https://codeat21.com/wp-content/uploads/2021/03/CodeAT21-Logo.png" />'
+        + '<meta property="og:url" content="https://codeat21.com/wp-content/uploads/2021/03/CodeAT21-Logo.png" />'
+        + '<meta property="og:type" content="article" />'
+        + '<meta http-equiv="refresh" content="1; url=https://codeat21.com/wp-content/uploads/2021/03/CodeAT21-Logo.png" />'
+        +'</head></html>';
+    return response.send(data);
 });
 
 
